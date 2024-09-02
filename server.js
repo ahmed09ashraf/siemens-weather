709e0f71b73d29815ef46474d091b1ef248bf3fb
el-laqqany-compeast <a.ibrahim@compeast.com>
2024-09-02 13:01:17 +0300

    Fix path to browser directory in server.js

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
-// Serve static files from the dist directory
-app.use(express.static(path.join(__dirname, 'dist/siemens-weather')));
+// Serve static files from the Angular app's 'browser' directory
+app.use(express.static(path.join(__dirname, 'dist/siemens-weather/browser')));
 
-// Serve index.html for all other routes, so Angular handles the routing
-app.get('/*', (req, res) =>
-  res.sendFile(path.join(__dirname, 'dist/siemens-weather/index.html'))
-);
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'dist/siemens-weather/browser/index.html'));
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
