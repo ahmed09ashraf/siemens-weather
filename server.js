e2ecd577d5331c55c9e59817e248893cbe4a1ba4
el-laqqany-compeast <a.ibrahim@compeast.com>
2024-09-02 12:34:35 +0300

    Setup server for Heroku deployment

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
 
 // Serve static files from the dist directory
 app.use(express.static(path.join(__dirname, 'dist/siemens-weather')));
 
-// Redirect all traffic to the index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/siemens-weather/index.html'));
-});
+// Serve index.html for all other routes, so Angular handles the routing
+app.get('/*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'dist/siemens-weather/index.html'))
+);
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8081);
+app.listen(process.env.PORT || 8080);
