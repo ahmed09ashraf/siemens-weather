ee7f9a9b191d137b86887f760021205abe92045c
el-laqqany-compeast <a.ibrahim@compeast.com>
2024-09-02 13:31:17 +0300

    Deploying production build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   weatherApiKey: '34ee60c7bdf24335a36104633242908',
   weatherApiBaseUrl: 'https://api.worldweatheronline.com/premium/v1'
 };
