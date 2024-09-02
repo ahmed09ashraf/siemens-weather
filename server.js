f39e69b00e86e4bb0680b70f0d3cdd293fef23c8
el-laqqany-compeast <a.ibrahim@compeast.com>
2024-09-02 12:30:37 +0300

    Setup server for Heroku deployment

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
 // Serve static files from the dist directory
 app.use(express.static(path.join(__dirname, 'dist/siemens-weather')));
 
-// Send all requests to index.html
-app.get('/*', function (req, res) {
+// Redirect all traffic to the index.html
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/siemens-weather/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8081);
